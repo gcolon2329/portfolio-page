b0de4641c9348255137d39f64c6fc67c429c9ff0
Gabe Colon <gabriel@letca.com>
2019-08-11 19:42:11 -0400

    Replaced all Lorem Ipsum and updated some css

--- styles/main.js
+++ styles/main.js
@@ -16,13 +16,13 @@ $(document).ready(function() {
 
     });
 
-    $('up').on('click', function(){
+    $('.up').on('click', function(){
         $('html, body').animate({
             scrollTop: 0
         }, 2000);
     });
 
-
+    
     AOS.init({
         easing: 'ease',
         duration: 1800,
