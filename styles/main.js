fee5bee79059051af650b020a82e484622e60316
Gabe Colon <gabriel@letca.com>
2019-09-14 17:59:57 -0400

    added hecho-en + new nav set up + more work links

--- styles/main.js
+++ styles/main.js
@@ -1,87 +1,81 @@
 $(document).ready(function() {
-    $('.menu-toggler').on('click', function() {
-        $(this).toggleClass('open');
-        $('.top-nav').toggleClass('open');
-    });
+  $(".menu-toggler").on("click", function() {
+    $(this).toggleClass("open");
+    $(".top-nav").toggleClass("open");
+  });
 
-    $('.top-nav .nav-link').on('click', function() {
-        $('.menu-toggler').removeClass('open');
-        $('.top-nav').removeClass('open');
-    });
+  $(".top-nav .nav-link").on("click", function() {
+    $(".menu-toggler").removeClass("open");
+    $(".top-nav").removeClass("open");
+  });
 
-    $('nav a[href*="#"]').on('click', function(){
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 100
-        }, 2000);
+  $('nav a[href*="#"]').on("click", function() {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top - 100
+      },
+      2000
+    );
+  });
 
-    });
+  $(".up").on("click", function() {
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      2000
+    );
+  });
 
-    $('.up').on('click', function(){
-        $('html, body').animate({
-            scrollTop: 0
-        }, 2000);
-    });
-
-    
-    AOS.init({
-        easing: 'ease',
-        duration: 1800,
-        once: true
-    });
-
+  AOS.init({
+    easing: "ease",
+    duration: 1800,
+    once: true
+  });
 });
 
-
-const texts = ['Designer','Developer','Creator','HTML','CSS','Javascript','Python'];
+const texts = [
+  "Designer",
+  "Developer",
+  "Creator",
+  "HTML",
+  "CSS",
+  "Javascript",
+  "Python"
+];
 let count = 0;
 let index = 0;
-let currentText = '';
-let letter = '';
+let currentText = "";
+let letter = "";
 
-(function type(){
-    if(count === texts.length){
-        count = 0;
-    }
-    currentText = texts[count];
-    letter = currentText.slice(0, ++index);
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
 
+  document.querySelector(".typing").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 400);
+})();
 
-    document.querySelector('.typing').textContent = letter;
-    if(letter.length === currentText.length){
-        count++;
-        index = 0;
-    }
-    setTimeout(type, 400);
-
-}());
-
-function bgChanger(){
-    if(this.scrollY > this.innerHeight / 2) {
-        document.body.classList.add('bg-active');
-    }else {
-        document.body.classList.remove('bg-active');
-    };
-
+function bgChanger() {
+  if (this.scrollY > this.innerHeight / 3) {
+    document.body.classList.add("bg-active");
+  } else {
+    document.body.classList.remove("bg-active");
+  }
 }
 
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-link');
-    const navLinks= document.querySelectorAll('.nav-link li');
-    //TOGGLE NAV
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-    });
+$(function() {
+  $(document).scroll(function() {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
 
-   
-}
-
-
-    window.addEventListener('scroll',bgChanger);
-    navSlide();
-    
-
-
-
-
-     
+window.addEventListener("scroll", bgChanger);
